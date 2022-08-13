5ef7dff7a89c551bc7e092e46625d37ca9df60a3
srikar <srikarsrivatsavvs@gmail.com>
2022-08-13 10:25:58 +0530

    App js edit for fun

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Editing the app js file for fun!
         </p>
         <a
           className="App-link"
